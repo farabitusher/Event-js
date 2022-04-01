e564969ed7124a01678f23ac57cf1a831e135109
farabitusher <61279854+farabitusher@users.noreply.github.com>
2022-04-01 19:40:35 +0600

    playing with dom in input area

--- event.js
+++ event.js
@@ -10,3 +10,12 @@ document.getElementById('greenbtn').addEventListener('click', function () {
 document.getElementById('yellowbtn').addEventListener('click', function () {
     document.body.style.backgroundColor='yellow'
 })
+// finding the input value by using javascript
+const input = document.getElementById('inputText');
+const p = document.getElementById('paragrap');
+
+
+const update = document.getElementById('updateBtn').addEventListener('click', function () {
+    p.innerText = input.value;
+    input.value= ""
+})
