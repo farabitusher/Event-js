7470a92f13c35878e8199c5daef7b8b5a6190516
farabitusher <61279854+farabitusher@users.noreply.github.com>
2022-04-01 19:21:25 +0600

    add event listner added

---
+++ event.js
@@ -0,0 +1,12 @@
+document.getElementById('redbtn').addEventListener('click', function () {
+    document.body.style.backgroundColor='red'
+})
+document.getElementById('bluebtn').addEventListener('click', function () {
+    document.body.style.backgroundColor='blue'
+})
+document.getElementById('greenbtn').addEventListener('click', function () {
+    document.body.style.backgroundColor='green'
+})
+document.getElementById('yellowbtn').addEventListener('click', function () {
+    document.body.style.backgroundColor='yellow'
+})
